69582f3e865868ec8bc24c0cf577a2684ad57c86
cauecbb <contato.cauecbb@gmail.com>
2025-07-20 22:55:53 +0200

    feat: Add unit tests for list_all_nodes view with pagination

--- challenge_hotiday/nodes/tests.py
+++ challenge_hotiday/nodes/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from .models import NodeTree, NodeTreeNames
+import json
 
 
 class NodeTreeModelTest(TestCase):
@@ -40,3 +41,53 @@ class NodeTreeModelTest(TestCase):
         # Test non-existent node
         name_nonexistent = NodeTreeNames.get_node_name(999, 'en')
         self.assertEqual(name_nonexistent, 'Node 999')
+
+
+class ListAllNodesViewTest(TestCase):
+    """Test cases for list_all_nodes view"""
+    
+    def setUp(self):
+        """Set up test data"""
+        self.factory = RequestFactory()
+        
+        # Create test nodes
+        self.root_node = NodeTree.objects.create(
+            lft=1, rgt=6, children_count=2
+        )
+        self.child_node = NodeTree.objects.create(
+            lft=2, rgt=3, children_count=0
+        )
+        
+        # Create names
+        NodeTreeNames.objects.create(
+            nodeTree=self.root_node, language='en', nodeName='Root'
+        )
+        NodeTreeNames.objects.create(
+            nodeTree=self.child_node, language='en', nodeName='Child'
+        )
+    
+    def test_list_all_nodes_success(self):
+        """Test successful listing of all nodes"""
+        from .views import list_all_nodes
+        
+        request = self.factory.get('/api/nodes/')
+        response = list_all_nodes(request)
+        
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        
+        self.assertEqual(data['status'], 'success')
+        self.assertEqual(len(data['data']['nodes']), 2)
+    
+    def test_list_all_nodes_with_pagination(self):
+        """Test pagination functionality"""
+        from .views import list_all_nodes
+        
+        request = self.factory.get('/api/nodes/', {'page_size': '1'})
+        response = list_all_nodes(request)
+        
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        
+        self.assertEqual(len(data['data']['nodes']), 1)
+        self.assertEqual(data['data']['pagination']['total_pages'], 2)
