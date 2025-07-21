8a4343f665af92b8e3623c43b36b037fa48d0f5b
cauecbb <contato.cauecbb@gmail.com>
2025-07-21 02:08:09 +0200

    fix: Correct POST method with a new URL

--- challenge_hotiday/nodes/urls.py
+++ challenge_hotiday/nodes/urls.py
@@ -5,8 +5,8 @@ app_name = 'nodes'
 
 urlpatterns = [
     # API endpoints
-    path('api/nodes/', views.list_all_nodes, name='list_all_nodes'),
+    path('api/nodes/', views.list_all_nodes, name='list_all_nodes'),  # GET
+    path('api/nodes/create/', views.create_node, name='create_node'),  # POST
     path('api/nodes/<int:node_id>/', views.get_node, name='get_node'),
     path('api/nodes/<int:node_id>/children/', views.search_children, name='search_children'),
-    path('api/nodes/', views.create_node, name='create_node'),
 ] 
